af68e03f80680ff961fbd108fc88bd7ae05c1cc2
João Bueno <joaovictorbuenodecaldas@gmail.com>
2023-08-17 09:54:30 -0300

    Screen segregation and routing test

---
+++ lib/routes/agendamento.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Agendamento extends StatefulWidget {
+  const Agendamento({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Agendamento> createState() => _AgendamentoState();
+}
+
+class _AgendamentoState extends State<Agendamento> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Número de agendamentos:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
