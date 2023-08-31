f96365fd0123ffb5e9025cbc4f5758e33030b9db
João Bueno <joaovictorbuenodecaldas@gmail.com>
2023-08-31 10:02:30 -0300

    Adicionado dialog de confirmação ao deletar consulta

--- lib/routes/consultas.dart
+++ lib/routes/consultas.dart
@@ -40,76 +40,104 @@ class _ConsultasState extends State<Consultas> {
                       itemBuilder: (context, index) {
                         return Card(
                           elevation: 8,
-                          margin: const EdgeInsets.only(right: 16, left: 16, bottom: 16),
+                          margin: const EdgeInsets.only(
+                              right: 16, left: 16, bottom: 16),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15),
                           ),
                           child: ListTile(
                             title: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('${snapshot.data![index].date},  ${snapshot.data![index].time}',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
-                                    color: Theme.of(context).colorScheme.secondary,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${snapshot.data![index].date},  ${snapshot.data![index].time}',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .secondary,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  snapshot.data![index].profissional,
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
+                                  Text(
+                                    snapshot.data![index].profissional,
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                                Text(snapshot.data![index].specialty,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color:
-                                        Theme.of(context).colorScheme.secondary,
-                                  )
-                                ),
-                                const SizedBox(height: 8),
-                              ]
-                            ),
-                            subtitle:Center(
+                                  Text(snapshot.data![index].specialty,
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                      )),
+                                  const SizedBox(height: 8),
+                                ]),
+                            subtitle: Center(
                               child: ElevatedButton(
                                 onPressed: () {
-                                  DBProvider.db
-                                  .deleteConsulta(snapshot.data![index]);
-                                  setState(() {
-                                    widget.consultas =
-                                    DBProvider.db.getAllConsultas();
-                                  });
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text('Cancelar consulta'),
+                                        content: const Text(
+                                            'Tem certeza que deseja cancelar a consulta?'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Não'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              DBProvider.db.deleteConsulta(
+                                                  snapshot.data![index]);
+                                              setState(() {
+                                                widget.consultas = DBProvider.db
+                                                    .getAllConsultas();
+                                              });
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Sim'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
                                 },
                                 style: ElevatedButton.styleFrom(
                                   fixedSize: const Size(120, 45),
-                                   backgroundColor:
-                                    Theme.of(context).colorScheme.secondary,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(30),
-                                      ),
-                                    ),
-                                    child: const Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Icon(Icons.free_cancellation),
-                                        SizedBox(width: 5),
-                                        Text('Cancelar', style: TextStyle(fontSize: 12)),
-                                     ],
-                                   ),
+                                  backgroundColor:
+                                      Theme.of(context).colorScheme.secondary,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
                                   ),
                                 ),
+                                child: const Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.free_cancellation),
+                                    SizedBox(width: 5),
+                                    Text('Cancelar',
+                                        style: TextStyle(fontSize: 12)),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
                         );
                       },
                     );
                   } else if (snapshot.hasError) {
                     return Text('${snapshot.error}');
-                  }
-                  else{
+                  } else {
                     return const Center(
-                      child: Text("Suas consultas aparecerão aqui",
+                      child: Text(
+                        "Suas consultas aparecerão aqui",
                         style: TextStyle(fontSize: 20),
                       ),
                     );
